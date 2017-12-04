0636809582ae64d996d0d77917a9ccdcd3de556b
Hunter Allen <hunter@openrobotics.org>
2017-12-04 16:41:03 -0500

    Ran ROS2 Linter (#5)
    * Linted goal_setter.cpp
    
    * Linted people_detector.cpp
    
    * "Are the red ones removed?"

--- src/goal_setter.cpp
+++ src/goal_setter.cpp
@@ -13,26 +13,27 @@ class GoalSetter//Map Reciever and Tourist Point List Generator
 {
 
 public:
-  GoalSetter()
-  {    
+  GoalSetter() {
     sub = n.subscribe("amcl_pose", 10, &GoalSetter::current_pos_retriever, this);
     first_spin = true;
     goal_index = -1;
     ros::spinOnce();  //spun for her pleasure..... and to get the initial position cause set_goal needs it
-    tpmap_reciever = n.serviceClient<hide_and_seek::tp_map>("generate_new_tp_list");  //this cant be right
+    tpmap_reciever = n.serviceClient < hide_and_seek::tp_map > ("generate_new_tp_list");  //this cant be right
     action_client("move_base", false);  //I'm not positive whether this should be true
-    while(!action_client.waitForServer( ros::Duration(5.0) ))
-    {
+    while (!action_client.waitForServer(ros::Duration(5.0) )) {
       ROS_INFO("Waiting for the move_base action server to come up");
     }  //in constructor so it doesnt wait 5 sec after every tourist point
     person_alert = n.subscribe("person_present", 10);
     this.set_goal();
   }
-      //prototypes bb
-  void GoalSetter::set_goal(hide_and_seek::tp_map_srv::Request &req, hide_and_seek::tp_map_srv::Response &res);
-  void current_pos_retriever(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& current_pos);
-  void GoalSetter::person_alert_callback(const std_msgs::Bool::ConstPtr& person_present_msg)
-  
+  //prototypes bb
+  void GoalSetter::set_goal(
+    hide_and_seek::tp_map_srv::Request & req,
+    hide_and_seek::tp_map_srv::Response & res);
+  void current_pos_retriever(
+    const geometry_msgs::PoseWithCovarianceStamped::ConstPtr & current_pos);
+  void GoalSetter::person_alert_callback(const std_msgs::Bool::ConstPtr & person_present_msg)
+
 private:
   ros::NodeHandle n;
   ros::ServiceClient tpmap_reciever;
@@ -40,45 +41,45 @@ private:
   geometry_msgs::PoseWithCovarianceStampedMessage current_position;
   ros::Subscriber sub;
   ros::Subscriber person_alert;
-  actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> action_client;
+  actionlib::SimpleActionClient < move_base_msgs::MoveBaseAction > action_client;
   move_base_msgs::MoveBaseGoal current_goal;
   hide_and_seek::tp_map_srv ottpm;  //The one true tourist point map
   bool first_spin;
   int goal_index;
 };
 
-void GoalSetter::current_pos_retriever(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& current_pos){  //I think the ConstPtr thing there is extraneous   LOL JK
+void GoalSetter::current_pos_retriever(
+  const geometry_msgs::PoseWithCovarianceStamped::ConstPtr & current_pos)                                       //I think the ConstPtr thing there is extraneous   LOL JK
+{
   this.current_position = current_pos;
-  if(first_spin)
-  {
+  if (first_spin) {
     first_spin = false;
     return;
-  }  
-  
-  if( (int)((current_position.pose.position.x - current_goal.pose.position.x) + (current_position.pose.position.y - current_goal.pose.position.y)) / PROXIMITY_THRESHOLD == 0)// i.e. if(close enough) Since they're(x and y) both held to the same standard of closeness, we can add them together and divide by that standard (technically this should be divided by 2 again but threshold was pretty large before
+  }
+
+  if ( (int)((current_position.pose.position.x - current_goal.pose.position.x) +
+    (current_position.pose.position.y - current_goal.pose.position.y)) / PROXIMITY_THRESHOLD == 0)                                                                            // i.e. if(close enough) Since they're(x and y) both held to the same standard of closeness, we can add them together and divide by that standard (technically this should be divided by 2 again but threshold was pretty large before
   {
-      this.set_goal();
+    this.set_goal();
   }
 }//JUST FINISHED THIS 12:36 AM December 2nd
 
-void GoalSetter::person_alert_callback(const std_msgs::Bool::ConstPtr& person_present_msg)
+void GoalSetter::person_alert_callback(const std_msgs::Bool::ConstPtr & person_present_msg)
 {
-  if(person_present_msg->data)
-  {
+  if (person_present_msg->data) {
     move_base_msgs::MoveBaseGoal goal;
     current_goal.target_pose.header.frame_id = "base_link";
     current_goal.target_pose.header.stamp = ros::Time::now();
   }
 }
 
-//call back function for 
+//call back function for
 
 void GoalSetter::set_goal()
-{ 
-  if(tpmap_reciever.call(ottpm))
-  {
+{
+  if (tpmap_reciever.call(ottpm)) {
 //find closest point and make that the new goal.
-    
+
     move_base_msgs::MoveBaseGoal goal;
     goal.target_pose.header.frame_id = "base_link";
     goal.target_pose.header.stamp = ros::Time::now();
@@ -86,45 +87,46 @@ void GoalSetter::set_goal()
     tp_at_current_location.tp_pos.x = current_position.pose.position.x;
     tp_at_current_location.tp_pos.y = current_position.pose.position.y;
     int index_of_closest_tp = 0;
-    float closest_distance = pow(pow((tp_at_current_location.tp_pos.x - ottpm.miniverse[i].tp_pos.x),2) + pow((tp_at_current_location.tp_pos.y - ottpm.miniverse[index_of_closest_tp].y), 2), .5);
-    if(ottpm.response.miniverse_size == 0)
-    {
+    float closest_distance =
+      pow(pow((tp_at_current_location.tp_pos.x - ottpm.miniverse[i].tp_pos.x),
+        2) + pow((tp_at_current_location.tp_pos.y - ottpm.miniverse[index_of_closest_tp].y), 2),
+        .5);
+    if (ottpm.response.miniverse_size == 0) {
       ROS_INFO("I checked the WHOLE ROOM. YOU WON?");//this seems like a dumb ending
       return;
     }
-    for(int i = 0; i < ottpm.response.miniverse_size; ++i)
-    {
-	hide_and_seek::tp_msg test_tp = ottpm.miniverse[i];
-	
-	if(test_tp.visited == true)
-	{
-	  continue;
-	}
-	float distance = pow(pow((tp_at_current_location.tp_pos.x - ottpm.miniverse[i].tp_pos.x),2) + pow((tp_at_current_location.tp_pos.y - ottpm.miniverse[index_of_closest_tp].y), 2), .5);
-	if(distance < closest_distance)
-	{
-	    index_of_closest_tp = i;
-	}
+    for (int i = 0; i < ottpm.response.miniverse_size; ++i) {
+      hide_and_seek::tp_msg test_tp = ottpm.miniverse[i];
+
+      if (test_tp.visited == true) {
+        continue;
+      }
+      float distance = pow(pow((tp_at_current_location.tp_pos.x - ottpm.miniverse[i].tp_pos.x),
+          2) +
+          pow((tp_at_current_location.tp_pos.y - ottpm.miniverse[index_of_closest_tp].y), 2), .5);
+      if (distance < closest_distance) {
+        index_of_closest_tp = i;
+      }
     }
-    
-    ottpm.request.miniverse = ottpm.response.miniverse;  
+
+    ottpm.request.miniverse = ottpm.response.miniverse;
     goal_index = index_of_closest_tp;
     current_goal.target_pose.pose.position.x = ottpm.response.miniverse[goal_index].tp_pos.x;
     current_goal.target_pose.pose.position.y = ottpm.response.miniverse[goal_index].tp_pos.y;
-    ROS_INFO("New goal chosen at x=%ld, y=%ld" (long int)current_goal.target_pose.pose.position.x, current_goal.target_pose.pose.position.y);
+    ROS_INFO("New goal chosen at x=%ld, y=%ld" (long int)current_goal.target_pose.pose.position.x,
+      current_goal.target_pose.pose.position.y);
     action_client.sendGoal(current_goal);
   }
   return true;
 }
 
-int main(int argc, char **argv)
+int main(int argc, char ** argv)
 {
   ros::init(argc, argv, "goal_setter");
 
   GoalSetter goalSetterster;
-  
+
   ros::spin();
 
   return 0;
 }
-
--- src/people_detector.cpp
+++ src/people_detector.cpp
@@ -10,24 +10,29 @@
 
 class PeopleFinder()
 {
- public:
-  PeopleFinder()  //maybe intake an integer number of players
-  {
+public:
+  PeopleFinder() { //maybe intake an integer number of players
     player_count = 1;
     found_count = 0;
     legs_found = false;
-    current_location_tracker = n.subscribe("amcl_pose", 10, &PeopleFinder::current_pos_retriever, this);
-    velocity_control = n.advertise<geometry_msgs::Twist>("/cmd_vel" , 10);
-    person_present_notifier = n.advertise<std_msgs::Bool>("person_present", 100);
-    person_leg_detector = n.subscribe("leg_detector/leg_tracker_measurements", 10, &legs_detect_callback, this);
-    person_face_detector = n.subscribe("face_detector/people_tracker_measurements", 10, &face_detect_callback, this);
+    current_location_tracker = n.subscribe("amcl_pose", 10, &PeopleFinder::current_pos_retriever,
+        this);
+    velocity_control = n.advertise < geometry_msgs::Twist > ("/cmd_vel", 10);
+    person_present_notifier = n.advertise < std_msgs::Bool > ("person_present", 100);
+    person_leg_detector = n.subscribe("leg_detector/leg_tracker_measurements", 10,
+        &legs_detect_callback, this);
+    person_face_detector = n.subscribe("face_detector/people_tracker_measurements", 10,
+        &face_detect_callback, this);
   }
 
-  void current_pos_retriever(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& current_pos);
-  void PeopleFinder::legs_detect_callback(const people_msgs::PositionMeasurementArray::ConstPtr& leg_place);
-  void PeopleFinder::face_detect_callback(const people_msgs::PositionMeasurementConstPtr& face_place);
-  
- private:
+  void current_pos_retriever(
+    const geometry_msgs::PoseWithCovarianceStamped::ConstPtr & current_pos);
+  void PeopleFinder::legs_detect_callback(
+    const people_msgs::PositionMeasurementArray::ConstPtr & leg_place);
+  void PeopleFinder::face_detect_callback(
+    const people_msgs::PositionMeasurementConstPtr & face_place);
+
+private:
   int player_count;
   int found_count;
   bool legs_found;
@@ -40,10 +45,10 @@ class PeopleFinder()
   geometry_msgs::PoseWithCovarianceStamped current_position;
 }
 
-void PeopleFinder::legs_detect_callback(const people_msgs::PositionMeasurementArray::ConstPtr& leg_place)
+void PeopleFinder::legs_detect_callback(
+  const people_msgs::PositionMeasurementArray::ConstPtr & leg_place)
 {
-  if(/*The people array is not empty */ )  //no legs afoot 
-  {
+  if (/*The people array is not empty */) { //no legs afoot
     //this.legs_detected
     legs_found = true;
     geometry_msgs::Twist vel_msg;
@@ -57,22 +62,20 @@ void PeopleFinder::legs_detect_callback(const people_msgs::PositionMeasurementAr
     person_present_notifier.publish(seen_msg);
     velocity_control.publish(vel_msg);
     ros::Duration(.1).sleep();
-    vel_msg.angular.z = .1;  
+    vel_msg.angular.z = .1;
     velocity_control.publish(vel_msg);  //turn around and around...
     ros::Duration(4.5).sleep();
     seen_msg.data = false;
     //TODO Add a random quote from a skyrim guard like "Must've been my imagination" to ROS_INFO as a flavorful "give up" text
     person_present_notifier.publish(seen_msg);
-    
+
   }
 }
 
-void PeopleFinder::face_detect_callback(const people_msgs::PositionMeasurementConstPtr& face_place)
+void PeopleFinder::face_detect_callback(const people_msgs::PositionMeasurementConstPtr & face_place)
 {
-  if(legs_found)  // I dont want it to have to find legs first, but structurally this is simpler rn
-  {
-    if(/*face place people array is not empty*/)
-    {
+  if (legs_found) { // I dont want it to have to find legs first, but structurally this is simpler rn
+    if (/*face place people array is not empty*/) {
       geometry_msgs::Twist vel_msg;  //I feel like an additional affirmation layer would be better to make sure there is someone really there
       vel_msg.linear.x = 0;
       vel_msg.linear.y = 0;
@@ -80,25 +83,26 @@ void PeopleFinder::face_detect_callback(const people_msgs::PositionMeasurementCo
       velocity_control.publish(vel_msg);
       ROS_INFO("I FOUND YOUUUUUUUUUU~~~~~~~~~~~");
       ++found_count;
-      if( found_count == player_count)
-      {
-	ROS_INFO("I WIN!!!!!!!!");
+      if (found_count == player_count) {
+        ROS_INFO("I WIN!!!!!!!!");
       }
     }
   }
 }
 
-void PeopleFinder::current_pos_retriever(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& current_pos){
+void PeopleFinder::current_pos_retriever(
+  const geometry_msgs::PoseWithCovarianceStamped::ConstPtr & current_pos)
+{
   this.current_position = current_pos->pose;
-} 
+}
 
-int main(int argc, char **argv)
+int main(int argc, char ** argv)
 {
   ros::init(argc, argv, "person_detector");
 
   PeopleFinder();
 
   ros::spin();
-  
+
   return 0;
 }
